18947f82dab5a09cb5830c668485af6a33a50bed
daisuke730 <89437189+daisuke730@users.noreply.github.com>
2022-09-06 19:24:39 +0900

    user

---
+++ user/todo_read.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+include("functions.php");
+check_session_id();
+
+$dbn = 'mysql:dbname=dec_todo;charset=utf8;port=3306;host=localhost';
+$user = 'root';
+$pwd = '';
+
+try {
+  $pdo = new PDO($dbn, $user, $pwd);
+} catch (PDOException $e) {
+  echo json_encode(["db error" => "{$e->getMessage()}"]);
+  exit();
+}
+
+$sql = 'SELECT * FROM todo_table ORDER BY url ASC';
+
+$stmt = $pdo->prepare($sql);
+$status = $stmt->execute();
+
+if ($status == false) {
+  $error = $stmt->errorInfo();
+  echo json_encode(["error_msg" => "{$error[2]}"]);
+  exit();
+} else {
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $output = "";
+  foreach ($result as $record) {
+    $output .= "
+      <tr>
+        
+        <td>{$record["todo"]}</td>
+        <td>{$record["url"]}</td>
+        
+      </tr>
+    ";
+  }
+  unset($record);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DB連携型todoリスト（一覧画面）</title>
+</head>
+
+<body>
+<fieldset>
+    <legend>DB連携型todoリスト（一覧画面）</legend>
+    <a href="todo_input.php">入力画面</a>
+    <a href="todo_logout.php">logout</a>
+    <table>
+      <thead>
+        <tr>
+        <th>行き先</th>
+          <th>url</th>
+          
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?= $output ?>
+      </tbody>
+    </table>
+  </fieldset>
+</body>
+
+</html>
