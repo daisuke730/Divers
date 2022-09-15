c990f8c1c26cd1f17522ac1277d39838bb781298
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-09-15 11:21:29 +0900

    change: ログインページを統合

--- src/todo_login.php
+++ src/todo_login.php
@@ -1,3 +1,44 @@
+<?php
+session_start();
+include('functions.php');
+
+$error_message = '';
+
+// ログインしようとしているかどうかを判定 (username, passwordがPOSTされているかどうか)
+if(isset($_POST['username']) && isset($_POST['password'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $pdo = connect_to_db();
+  $sql = 'SELECT * FROM users_table WHERE username=:username AND password=:password AND is_deleted=0';
+  $stmt = $pdo->prepare($sql);
+  $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+  $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+  $status = $stmt->execute();
+
+  // エラーであれば終了
+  if ($status == false) {
+    $error = $stmt->errorInfo();
+    echo json_encode(["error_msg" => "{$error[2]}"]);
+    exit();
+  }
+
+  $val = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  // ログインに成功した場合
+  if ($val) {
+    $_SESSION = array();
+    $_SESSION['session_id'] = session_id();
+    $_SESSION['is_admin'] = $val['is_admin'];
+    $_SESSION['username'] = $val['username'];
+    header("Location:todo_read.php");
+    exit();
+  }
+
+  $error_message = 'ユーザ名かパスワードに誤りがあります';
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -8,9 +49,10 @@
 </head>
 
 <body>
-  <form action="todo_login_act.php" method="POST">
+  <form method="POST">
     <fieldset>
       <legend>todoリストログイン画面</legend>
+      <?php if ($error_message) echo "<p>{$error_message}</p>" ?>
       <div>
         username: <input type="text" name="username">
       </div>
