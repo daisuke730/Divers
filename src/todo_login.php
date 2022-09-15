03c99c581ac91b0519222715d89544023a73313d
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-09-15 19:22:43 +0900

    change: ログイン・新規登録ページのデザインを変更

--- src/todo_login.php
+++ src/todo_login.php
@@ -45,23 +45,38 @@ include("components/head.php");
 ?>
 
 <body>
-  <form method="POST">
-    <fieldset>
-      <legend>todoリストログイン画面</legend>
-      <?php if ($error_message) echo "<p>{$error_message}</p>" ?>
+  <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="w-full max-w-md space-y-8">
       <div>
-        username: <input type="text" name="username">
+        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">ログイン</h2>
       </div>
+      <?php if ($error_message) echo '
+      <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+        <span class="block sm:inline">'. $error_message .'</span>
+      </div>'
+      ?>
+      <form class="mt-8 space-y-6" method="POST">
+        <div class="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label for="username" class="sr-only">ユーザー名</label>
+            <input id="username" name="username" type="text" autocomplete="email" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="ユーザー名">
+          </div>
+          <div>
+            <label for="password" class="sr-only">パスワード</label>
+            <input id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="パスワード">
+          </div>
+        </div>
+        <div>
+          <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            ログイン
+          </button>
+        </div>
+      </form>
       <div>
-        password: <input type="text" name="password">
+        <p class="mt-6 text-center tracking-tight text-gray-900">または<a href="todo_register.php" class="text-indigo-500 text-bold">新規登録</a></p>
       </div>
-      <div>
-        <button>Login</button>
-      </div>
-      <a href="todo_register.php">or register</a>
-    </fieldset>
-  </form>
-
+    </div>
+  </div>
 </body>
 
-</html>
+<?php include("components/footer.php"); ?>
--- src/todo_register.php
+++ src/todo_register.php
@@ -52,22 +52,38 @@ include("components/head.php");
 ?>
 
 <body>
-  <form method="POST">
-    <fieldset>
-      <legend>todoリストユーザ登録画面</legend>
-      <?php if ($error_message) echo "<p>{$error_message}</p>" ?>
+  <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="w-full max-w-md space-y-8">
       <div>
-        username: <input type="text" name="username">
+        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">新規登録</h2>
       </div>
+      <?php if ($error_message) echo '
+      <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+        <span class="block sm:inline">'. $error_message .'</span>
+      </div>'
+      ?>
+      <form class="mt-8 space-y-6" method="POST">
+        <div class="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label for="username" class="sr-only">ユーザー名</label>
+            <input id="username" name="username" type="text" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="ユーザー名">
+          </div>
+          <div>
+            <label for="password" class="sr-only">パスワード</label>
+            <input id="password" name="password" type="password" required class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="パスワード">
+          </div>
+        </div>
+        <div>
+          <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            登録
+          </button>
+        </div>
+      </form>
       <div>
-        password: <input type="text" name="password">
+        <p class="mt-6 text-center tracking-tight text-gray-900">既にアカウントがある場合は<a href="todo_login.php" class="text-indigo-500 text-bold">こちらからログイン</a></p>
       </div>
-      <div>
-        <button>Register</button>
-      </div>
-      <a href="todo_login.php">or login</a>
-    </fieldset>
-  </form>
+    </div>
+  </div>
 </body>
 
 <?php include("components/footer.php"); ?>
