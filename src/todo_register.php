02735a7213cf247344a1ca0baac6818d4634f323
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-09-15 11:50:52 +0900

    change: 登録ページを統合

--- src/todo_register.php
+++ src/todo_register.php
@@ -1,3 +1,51 @@
+<?php
+include('functions.php');
+
+function register() {
+  // POSTリクエストでなければ終了
+  if($_SERVER['REQUEST_METHOD'] !== 'POST') return;
+
+  // 入力チェック
+  $isValidInput = isset($_POST['username']) && $_POST['username'] !== '' && isset($_POST['password']) && $_POST['password'] !== '';
+  if (!$isValidInput) return 'ユーザ名またはパスワードが入力されていません。';
+
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  $pdo = connect_to_db();
+  $sql = 'SELECT COUNT(*) FROM users_table WHERE username=:username';
+  $stmt = $pdo->prepare($sql);
+  $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+  $status = $stmt->execute();
+
+  if ($status == false) {
+    $error = $stmt->errorInfo();
+    echo json_encode(["error_msg" => "{$error[2]}"]);
+    exit();
+  }
+
+  // すでに登録されていた場合
+  if ($stmt->fetchColumn() > 0) return '既に登録されているユーザーです。';
+
+  $sql = 'INSERT INTO users_table(id, username, password, is_admin, is_deleted, created_at, updated_at) VALUES(NULL, :username, :password, 0, 0, sysdate(), sysdate())';
+  $stmt = $pdo->prepare($sql);
+  $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+  $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+  $status = $stmt->execute();
+
+  if ($status == false) {
+    $error = $stmt->errorInfo();
+    echo json_encode(["error_msg" => "{$error[2]}"]);
+    exit();
+  }
+
+  header("Location:todo_login.php");
+  exit();
+}
+
+$error_message = register();
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -8,9 +56,10 @@
 </head>
 
 <body>
-  <form action="todo_register_act.php" method="POST">
+  <form method="POST">
     <fieldset>
       <legend>todoリストユーザ登録画面</legend>
+      <?php if ($error_message) echo "<p>{$error_message}</p>" ?>
       <div>
         username: <input type="text" name="username">
       </div>
