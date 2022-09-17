b13fc20bce405467fef8a51e9ee81198e3d2d0bf
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-09-17 17:04:20 +0900

    fix: ルートディレクトリ以外からでは読み込めない問題を修正

--- src/components/head.php
+++ src/components/head.php
@@ -5,5 +5,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
