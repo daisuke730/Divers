b1dd357cf28116f50e96d568de1b41dab37431e0
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-09-17 16:47:40 +0900

    change: 使用するライブラリを変更

--- src/components/footer.php
+++ src/components/footer.php
@@ -1,2 +1,3 @@
-<script src="https://cdn.tailwindcss.com"></script>
+<script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
 </html>
--- src/components/head.php
+++ src/components/head.php
@@ -4,5 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
