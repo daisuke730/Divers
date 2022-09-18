f64c8b41f553bbb77af0077c455ea82d17032a14
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-09-18 13:53:30 +0900

    change: APIの構造を変更

--- src/api/action.php
+++ src/api/action.php
@@ -16,6 +16,33 @@ if(!isset($_GET['q']) && !isset($_POST['q'])) {
 
 $pdo = connect_to_db();
 
+// 投稿IDからいいね数といいね状態を取得
+function get_likes($post_id, $user_id) {
+  global $pdo;
+
+  // いいね数を取得
+  $sql = "SELECT COUNT(*) AS likes FROM likes WHERE post_id = :post_id";
+  $stmt = $pdo->prepare($sql);
+  $stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
+  $status = $stmt->execute();
+  db_error_check($status, $stmt);
+  $likes = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  // いいね状態を取得
+  $sql = "SELECT COUNT(*) AS liked FROM likes WHERE post_id = :post_id AND user_id = :user_id";
+  $stmt = $pdo->prepare($sql);
+  $stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
+  $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+  $status = $stmt->execute();
+  db_error_check($status, $stmt);
+  $liked = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  return [
+    'like_count' => $likes['likes'],
+    'is_liked' => $liked['liked'] > 0
+  ];
+}
+
 // POSTリクエスト
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
   switch($_POST['q']) {
@@ -79,38 +106,31 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 // GETリクエスト
 if($_SERVER['REQUEST_METHOD'] === 'GET') {
   switch($_GET['q']) {
-    // 投稿のいいね数および自分がいいねしているかを取得
-    case 'getPostLikes': {
-      if (!isset($_GET['post_id'])) {
-        http_response_code(400);
-        exit();
-      }
-
-      $post_id = $_GET['post_id'];
-      $sql = "SELECT user_id FROM likes WHERE post_id = :post_id";
-      $stmt = $pdo->prepare($sql);
-      $stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
-      $stmt->execute();
-      $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-      $res = [
-        'likes' => count($result),
-        'isLiked' => in_array($_SESSION['user_id'], array_column($result, 'user_id'))
-      ];
-
-      echo json_encode($res);
-      exit();
-    }
-
     // 投稿一覧を取得
-    // pageクエリでオフセットを設定可能 (20件ずつ)
+    // - pageクエリでオフセットを設定可能 (20件ずつ)
+    // - searchクエリで投稿を検索可能
     case 'getPosts': {
-      $sql = "SELECT * FROM todo_table ORDER BY updated_at DESC LIMIT 50 OFFSET :offset";
+      $keyword = isset($_GET['search']) ? $_GET['search'] : '';
+
+      $sql = "SELECT * FROM todo_table WHERE todo LIKE :keyword ORDER BY updated_at DESC LIMIT 20 OFFSET :offset";
       $stmt = $pdo->prepare($sql);
       $offset = isset($_GET['page']) ? (max((int)$_GET['page'], 1) - 1) * 20 : 0;
+      $stmt->bindValue(':keyword', "%{$keyword}%", PDO::PARAM_STR);
       $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
       $stmt->execute();
       $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+      foreach($result as &$post) {
+        // いいね数と自分がいいねしているかを取得
+        $likeState = get_likes($post['id'], $_SESSION['user_id']);
+
+        // リザルトと統合
+        $post = array_merge($post, $likeState);
+
+        // この投稿を編集できるかどうか
+        $post['can_edit'] = $post['user_id'] === $_SESSION['user_id'] || $_SESSION['is_admin'] === 1;
+      }
+
       echo json_encode($result);
       exit();
     }
