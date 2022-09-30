d367afd41b2627f2f64d29d15346ea262a4e0c97
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-09-30 21:01:02 +0900

    add: スマホ向けメニューを追加

--- src/components/footer.php
+++ src/components/footer.php
@@ -4,5 +4,6 @@
 
         <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
+        <script type="text/javascript" src="/js/hamburger.js"></script>
     </body>
 </html>
--- src/components/head.php
+++ src/components/head.php
@@ -6,14 +6,18 @@
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
     <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/hamburger.css">
 </head>
 
 <body <?php if(isset($bgColor) && $bgColor) echo 'class="body-bg"'; ?>>
-    <div class="ui secondary menu">
-        <img class="item logo-header" src="/img/logo.png" alt="logo">
+    <div class="ui secondary menu stackable">
+        <img class="item header logo-header" src="/img/logo.png" alt="logo">
         <a class="item" href="/">トップページ</a>
         <a class="item" href="/posts">投稿一覧</a>
         <div class="right menu">
             <a class="ui item" href="<?= is_loggedin() ? '/logout.php' : '/login.php' ?>"><?= is_loggedin() ? 'ログアウト' : 'ログイン' ?></a>
+        </div>
+        <div class="hamburger">
+            <i id="hamburger-toggle" class="bars icon"></i>
         </div>
     </div>
