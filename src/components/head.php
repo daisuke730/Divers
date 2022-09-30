f5302287d7e517f25fe0457566c0d84ac863a29d
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-10-01 01:05:50 +0900

    add: 管理者の場合はメニューにダッシュボードへのリンクを表示するように

--- src/components/head.php
+++ src/components/head.php
@@ -14,6 +14,7 @@
         <img class="item header logo-header" src="/img/logo.png" alt="logo">
         <a class="item" href="/">トップページ</a>
         <a class="item" href="/posts">投稿一覧</a>
+        <?php if (is_admin()) echo '<a class="item" href="/admin/dashboard.php">ダッシュボード</a>' ?>
         <div class="right menu">
             <a class="ui item" href="<?= is_loggedin() ? '/logout.php' : '/login.php' ?>"><?= is_loggedin() ? 'ログアウト' : 'ログイン' ?></a>
         </div>
