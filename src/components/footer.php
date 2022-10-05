ce16777bf41ca6e26a2afeeefc02c79a3a91fff9
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-10-06 00:33:54 +0900

    add: コンポーネントの呼び出し元からcss/jsを設定出来るように

--- src/components/footer.php
+++ src/components/footer.php
@@ -4,6 +4,13 @@
 
         <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
-        <script type="text/javascript" src="/js/hamburger.js"></script>
+        <script src="/js/hamburger.js"></script>
+        <?php
+            if (isset($js)) {
+                foreach ($js as $child) {
+                    echo "<script src=\"{$child}\"></script>";
+                }
+            }
+        ?>
     </body>
 </html>
--- src/components/head.php
+++ src/components/head.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="/css/reset.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/hamburger.css">
+    <?php
+        if (isset($css)) {
+            foreach ($css as $child) {
+                echo "<link rel=\"stylesheet\" href=\"{$child}\">";
+            }
+        }
+    ?>
 </head>
 
 <body <?php if(isset($bgColor) && $bgColor) echo 'class="body-bg"'; ?>>
@@ -14,6 +22,7 @@
         <img class="item header logo-header" src="/img/logo.png" alt="logo">
         <a class="item" href="/">トップページ</a>
         <a class="item" href="/posts">投稿一覧</a>
+        <a class="item" href="/howtouse.php">使い方</a>
         <?php if (is_admin()) echo '<a class="item" href="/admin/dashboard.php">ダッシュボード</a>' ?>
         <div class="right menu">
             <a class="ui item" href="<?= is_loggedin() ? '/logout.php' : '/login.php' ?>"><?= is_loggedin() ? 'ログアウト' : 'ログイン' ?></a>
