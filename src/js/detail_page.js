ceadf3b5cc6874073a5214bb6e279988b70b9d8f
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-10-27 18:38:42 +0900

    style: 投稿詳細ページのデザインを調整

--- src/js/detail_page.js
+++ src/js/detail_page.js
@@ -5,12 +5,27 @@ window.onload = () => {
 async function getPostData() {
     let id = new URLSearchParams(location.search).get('id')
     let params = {id}
-    let res = await api('GET', 'getPost', params)
-    console.log(res)
+    let res = await api('GET', 'getPost', params, false)
+
+    if (res.error) {
+        $('#error-message').text(res.error)
+        $('#error-message').removeClass('hidden')
+        $('#detail').addClass('hidden')
+        return
+    }
 
     $('#post-title').text(res.name)
     $('#post-created-at').text(res.created_at)
     $('#post-updated-at').text(res.updated_at)
     $('#post-url').attr('href', res.url)
     $('#like-button').html(getLikeButtonTemplate(res.id, res.is_liked, res.like_count, true, true))
+    $('#route-image').attr('src', `/api/image.php?id=${res.id}`)
+    $('#route-distance').text(distanceBeautify(res.distance))
+    $('#route-duration').text(durationBeautify(res.duration))
+
+    if (res.description) {
+        $('#post-description').text(res.description)
+    } else {
+        $('#post-description-box').addClass('hidden')
+    }
 }
