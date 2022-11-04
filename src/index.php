8e1f93928f67c2fd7392913dc97f4d2d71e1ed72
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-11-05 00:51:05 +0900

    feat: トップページを最新版に更新

--- src/index.php
+++ src/index.php
@@ -6,41 +6,46 @@ include('functions.php');
 <?php
 $title = "DiversMap";
 $bgColor = false;
-$css = ["/css/top/style_tyarekyara.css", "https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"];
+$css = ['https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/css/splide.min.css', '/css/top_page.css'];
 include("components/head.php");
 ?>
-<div class="overlay">
-  <div class="slider_wrap">
-    <ul class="slider">
-      <li><div class="full-img" style="background-image: url(/img/top/image1.jpg)"></div></li>
-      <li><div class="full-img" style="background-image: url(/img/top/image2.jpg)"></div></li>
-      <li><div class="full-img" style="background-image: url(/img/top/image3.jpg)"></div></li>
-      <li><div class="full-img" style="background-image: url(/img/top/image4.jpg)"></div></li>
-    </ul>
 
-    <div class="thumbnail">
-      <li class="thumbnail-img"><img src="/img/top/image1.jpg"></li>
-      <li class="thumbnail-img"><img src="/img/top/image2.jpg"></li>
-      <li class="thumbnail-img"><img src="/img/top/image3.jpg"></li>
-      <li class="thumbnail-img"><img src="/img/top/image4.jpg"></li>
+<div class="head-container">
+    <!-- <div id="slide" class="splide" role="group" aria-label="">
+        <div class="splide__track">
+            <ul class="splide__list">
+                <li class="splide__slide"><div class="slider-image" style="background-image: url(/img/top/image1.jpg)"></li>
+                <li class="splide__slide"><div class="slider-image" style="background-image: url(/img/top/image2.jpg)"></li>
+                <li class="splide__slide"><div class="slider-image" style="background-image: url(/img/top/image3.jpg)"></li>
+                <li class="splide__slide"><div class="slider-image" style="background-image: url(/img/top/image4.jpg)"></li>
+            </ul>
+        </div>
+    </div> -->
+    <img src="/img/logo_top.png" alt="logo" class="head-logo">
+    <h3>みんなで作る、ルート共有アプリ</h3>
+    <div class="horizon button-container">
+        <a class="ui button yellow big" href="/posts">ルートを見てみる</a>
+        <a class="ui button white big" href="/howtouse.php">使い方</a>
     </div>
-  </div>
-  <div class="contents">
-    <a class="ui button big teal" href="/posts">ルート一覧はこちら</a>
-    <div class="responsive-container" style="margin-top: 32px;">
-      <a class="ui button" href="/register.php">新規登録</a>
-      <span>または</span>
-      <a class="ui button" href="/login.php">ログイン</a>
-    </div>
-    <div class="ui info message" style="margin-top: 32px;">
-      <a href="/howtouse.php">使い方はこちらをご覧ください</a>
-    </div>
-    <p>画像提供: 福岡市</p>
-  </div>
 </div>
 
-<!--これはジャバスクリプトの読み込み-->
+<div class="contents-container reverse">
+    <img src="/img/top/mockup_route_list.png">
+    <div>
+        <h2>DiversMapとは？</h2>
+        <p>DiversMapは、足が不自由な方や車いすを利用されている方のほか、ベビーカーをお持ちの方・お年寄りの方など、<br>様々な方々が段差の多い道などの"通りづらい道"を避けて通れるルートを共有できるアプリです。</p>
+    </div>
+</div>
+
+<div class="contents-container">
+    <div>
+        <h2>誰でも、簡単に。</h2>
+        <p>ルートは誰でも作成することができ、出発地と目的地を入力するだけでOK。<br>表示されたルートに通りづらい場所がある場合は、簡単にその場所を避けたルートに変更することができます。</p>
+    </div>
+    <img src="/img/top/mockup_route_post.png">
+</div>
+
 <?php
-$js = ["https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js", "/js/top/main_screen1.js"];
+$js = ['https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/js/splide.min.js', '/js/top_page.js'];
 include("components/footer.php");
 ?>
---
+++ src/js/top_page.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    // new Splide('#slide', {
+    //     type: 'fade',
+    //     arrows: false,
+    //     autoplay: true,
+    //     drag: false,
+    //     pagination: false,
+    // }).mount()
+}
